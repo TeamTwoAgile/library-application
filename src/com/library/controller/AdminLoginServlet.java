ecd01eaa52db8060a9a78df45b8c060df2045e99
Damien Hunter <a00014290@student.ait.ie>
2021-11-10 13:29:20 +0000

    modified:   src/com/library/controller/AdminLoginServlet.java 	WebContent/WEB-INF/views/adminlogin.jsp

--- src/com/library/controller/AdminLoginServlet.java
+++ src/com/library/controller/AdminLoginServlet.java
@@ -17,5 +17,13 @@ public class AdminLoginServlet extends HttpServlet{
 		super();
 	}
 	
-	
+	public void init(ServletConfig config) throws ServletException{
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/adminlogin.jsp");
+		dispatcher.forward(request,  response);
+	}
 }
