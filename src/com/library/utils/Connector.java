03e72998eb3843422d39bd142dddb28121e45a23
Damien Hunter <a00014290@student.ait.ie>
2021-11-19 10:00:42 +0000

    Book Checkout Return Functionality Addition

--- src/com/library/utils/Connector.java
+++ src/com/library/utils/Connector.java
@@ -57,6 +57,42 @@ public class Connector{
 		return isAvailable;
 	}
 	
+	public static void bookCheckOut(){
+		boolean isAvailable = false;
+		
+		try{
+			Class.forName("org.mariadb.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/project", "root", "D7i4FjL10!");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM book_available");
+
+			while(rs.next()){
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3) + " " + rs.getString(4) + " " + rs.getString(5) + " " + rs.getInt(6) + " " + rs.getString(7) + " " + rs.getInt(8));
+				
+			}
+			con.close();
+		}
+		catch(Exception e){}
+	}
+	
+	public static void bookReturn(){
+		boolean isAvailable = false;
+		
+		try{
+			Class.forName("org.mariadb.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/project", "root", "D7i4FjL10!");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM book_available");
+
+			while(rs.next()){
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3) + " " + rs.getString(4) + " " + rs.getString(5) + " " + rs.getInt(6) + " " + rs.getString(7) + " " + rs.getInt(8));
+				
+			}
+			con.close();
+		}
+		catch(Exception e){}
+	}
+	
 	public static void main(String [] args){
 		Connector.bookAvailable();
 	}
