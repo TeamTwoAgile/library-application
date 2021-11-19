ece775cc5f53aedb81fe868db83afc42cea7aafc
Damien Hunter <a00014290@student.ait.ie>
2021-11-19 09:58:46 +0000

    Book Checkout Return Functionality

--- src/com/library/utils/Connector.java
+++ src/com/library/utils/Connector.java
@@ -30,4 +30,34 @@ public class Connector{
 		}
 		return loggedIn;
 	}
+	
+	public static boolean bookAvailable(){
+		boolean isAvailable = false;
+		
+		try{
+			Class.forName("org.mariadb.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/project", "root", "D7i4FjL10!");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM book_available");
+
+			while(rs.next()){
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getInt(3) + " " + rs.getString(4) + " " + rs.getString(5) + " " + rs.getInt(6) + " " + rs.getString(7) + " " + rs.getInt(8));
+				
+				if(rs.getInt(0) == 1){
+					isAvailable = true;
+				}
+				else{
+					isAvailable = false;
+				}
+			}
+			con.close();
+		}
+		catch(Exception e){}
+		
+		return isAvailable;
+	}
+	
+	public static void main(String [] args){
+		Connector.bookAvailable();
+	}
 }
