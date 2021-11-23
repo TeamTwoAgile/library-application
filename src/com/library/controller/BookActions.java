11e3b1cd7a799956dc2bce94a7fc952a37bb6375
Damien Hunter <a00014290@student.ait.ie>
2021-11-23 13:42:18 +0000

    new file:   src/com/library/controller/BookActions.java

---
+++ src/com/library/controller/BookActions.java
@@ -0,0 +1,18 @@
+package com.library.controller;
+
+import java.io.IOException;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "bookActions", urlPatterns = { "/bookActions" })
+public class BookActions{
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/views/bookactions.jsp");
+		dispatcher.forward(request, response);
+	}
+}
