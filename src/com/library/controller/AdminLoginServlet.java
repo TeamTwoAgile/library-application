76653a5da080ba31fbe1cbb08f21ed7c2071e478
Damien Hunter <a00014290@student.ait.ie>
2021-11-23 11:08:31 +0000

    modified:   src/com/library/controller/AdminLoginServlet.java

--- src/com/library/controller/AdminLoginServlet.java
+++ src/com/library/controller/AdminLoginServlet.java
@@ -27,7 +27,7 @@ public class AdminLoginServlet extends HttpServlet{
 		boolean loggedIn = Connector.verifyAdmin(a.getUser(), a.getPass());
 
 		if(loggedIn == true){
-			response.sendRedirect("WEB-INF/views/dashboard.jsp");
+			response.sendRedirect("WEB-INF/views/admin_homepage.jsp");
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/AdminDashboardServlet");
 			dispatcher.forward(request,  response);
 		}
