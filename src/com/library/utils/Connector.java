b4c842f5c42455f1a69e991ad50bea492910b4e0
Damien Hunter <a00014290@student.ait.ie>
2021-11-24 10:37:10 +0000

    modified:   src/com/library/utils/Connector.java

--- src/com/library/utils/Connector.java
+++ src/com/library/utils/Connector.java
@@ -52,7 +52,7 @@ public class Connector{
 		return isAvailable;
 	}
 	
-	public static void findBook(String name, String author){
+	public static void findBook(String name, String author, String check){
 		try{
 			Class.forName("org.mariadb.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/project", "root", "D7i4FjL10!");
@@ -60,7 +60,12 @@ public class Connector{
 			ResultSet rs = stmt.executeQuery("SELECT * FROM book_available where author = '" + author + "'");
 			
 			while(rs.next()){
-				
+				if(check.equals("on")){
+					bookReturn(rs.getInt(1));
+				}
+				else {
+					bookCheckOut(rs.getInt(1));
+				}
 			}
 		}
 		catch(Exception e){}
